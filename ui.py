8fb4ff7fc9929f47e80f4325acc10386862e00c3
Ola Krassowska <ola.krassowska@hotmail.com>
2024-08-26 14:14:09 +0200

    Organizing and clearling code, added limit_level animation on max block level

--- ui.py
+++ ui.py
@@ -1,5 +1,5 @@
 from ursina import Text
-from config import money, color, current_district_info
+from config import *
 
 money_text = Text(
     text=f'${money}',
@@ -18,3 +18,54 @@ district_info_text = Text(
     scale=1.5,
     anchor=('left', 'top')
 )
+
+def update_money_text():
+    money_text.text = f'${int(money)}'
+
+def update_district_info(district_id):
+    global current_district_info
+    if district_id in districts:
+        district = districts[district_id]
+        if district['value'] > 200:
+            district['maxHeight'] = 2
+        if district['value'] > 1000:
+            district['maxHeight'] = 3
+        if district['value'] > 3000:
+            district['maxHeight'] = 4
+        if district['value'] > 6000:
+            district['maxHeight'] = 5
+        if district['value'] > 10000:
+            district['maxHeight'] = 6
+
+        current_district_info = f"District ID: {district['id']}\nValue: {district['value']}\nMax Height: {district['maxHeight']}\nPeople: {district['people']}"
+        district_info_text.text = current_district_info
+    else:
+        district_info_text.text = ""
+        
+        
+def update_district(x_start, z_start, color):
+    chunk_id = (x_start, z_start)
+    if chunk_id in districts:
+        if color == default_color:
+            districts[chunk_id]['value'] += 20
+            districts[chunk_id]['people'] += 100
+        elif color == selected_color:
+            districts[chunk_id]['value'] += 50
+            districts[chunk_id]['people'] += 150
+        elif color == selected_color_b:
+            districts[chunk_id]['value'] += 150
+            districts[chunk_id]['people'] += 200
+        elif color == selected_color_c:
+            districts[chunk_id]['value'] += 350
+            districts[chunk_id]['people'] += 250
+        elif color == selected_color_d:
+            districts[chunk_id]['value'] += 550
+            districts[chunk_id]['people'] += 300
+        elif color == selected_color_e:
+            districts[chunk_id]['value'] += 750
+            districts[chunk_id]['people'] += 350
+        elif color == selected_color_f:
+            districts[chunk_id]['value'] += 1000
+            districts[chunk_id]['people'] += 400
+
+        update_district_info(chunk_id)
